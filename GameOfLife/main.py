e3f95716a5df1d1fdc0c6800152dc39c12d9c966
Kyle Smith <ksmith126@ycp.edu>
2021-02-21 11:42:46 -0500

    update

--- GameOfLife/main.py
+++ GameOfLife/main.py
@@ -2,15 +2,6 @@ from GameOfLife.Cell import Cell
 width = 9
 height = 9
 
-arr = [Cell for i in range(width)]
-
-for i in range(width):
-    stat = True
-    if i/2 == 1:
-        stat = True
-    else:
-        stat = False
-    arr[i] = Cell.__init__(arr[i], 1, 1, stat)
 
 
 cell = Cell
@@ -19,7 +10,3 @@ cell.__init__(cell, 5, 7, False)
 
 
 print(cell.getx(cell), cell.gety(cell), cell.getStatus(cell))
-
-for i in range(width):
-    print(arr[i].getStatus())
-
