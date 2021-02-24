a831158ad572ce1772f04b98330af35ed8c7f43f
Kyle Smith <ksmith126@ycp.edu>
2021-02-24 17:32:53 -0500

    Basic game logic nearly completed

--- GameOfLife/main.py
+++ GameOfLife/main.py
@@ -26,3 +26,71 @@ for i in range(5):
             print("O", end=' ')
         else:
             print("X", end=' ')
+
+
+for i in range(5):
+    print("i loop")
+    for j in range(5):
+        print("j loop")
+        lifeCount = 0
+        # helper variables to get the bound of area around cell
+        # if/else keep from trying to access elements that are outside of bounds
+
+        # bounds for x-axis
+        if board0[i][j].getx() > 0:
+            tempWidthStart = board0[i][j].getx() - 1
+        else:
+            tempWidthStart = board0[i][j].getx()
+        if board0[i][j].getx() < 4:
+            tempWidthEnd = board0[i][j].getx() + 1
+        else:
+            tempWidthEnd = board0[i][j].getx()
+
+        # bounds for y-axis
+        if board0[i][j].gety() > 0:
+            tempHeightStart = board0[i][j].gety() - 1
+        else:
+            tempHeightStart = board0[i][j].gety()
+        if board0[i][j].gety() < 4:
+            tempHeightEnd = board0[i][j].gety() + 1
+        else:
+            tempHeightEnd = board0[i][j].gety()
+
+        # nested loops that check the number of surrounding cells
+        while tempWidthStart <= tempWidthEnd:
+            print("width loop")
+
+            # height loop for surrounding cells
+            while tempHeightStart <= tempHeightEnd:
+                print("height loop")
+
+                # keeps from including the current cell in lifeCount
+                if board0[i][j].getx() != i & board0[i][j].gety() != j:
+
+                    # if a neighbor cell is alive it is added to lifeCount
+                    if board0[tempWidthStart][tempHeightStart].getStatus():
+                        lifeCount = lifeCount + 1
+                        print(lifeCount)
+                tempHeightStart += 1
+
+                
+                if lifeCount == 3 | lifeCount == 2:
+                    board1[i][j] = Cell(i, j, True)
+
+                else:
+                    board1[i][j] = Cell(i, j, False)
+            tempWidthStart += 1
+
+for i in range(5):
+    print()
+
+    for j in range(5):
+
+        if board1[i][j].getStatus():
+            print("O", end=' ')
+
+        else:
+            print("X", end=' ')
+
+
+
