fedef3bf3e322871964a9e0a91f11691c321aa48
Kyle Smith <ksmith126@ycp.edu>
2021-03-05 14:22:16 -0500

    Fully competed Board class

--- GameOfLife/Board.py
+++ GameOfLife/Board.py
@@ -2,18 +2,26 @@ from GameOfLife.Cell import Cell
 
 import numpy as np
 
+import random
+
 
 class Board:
 
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        array = np.empty((width, height), Cell)
+        self.array = np.empty((width, height), Cell)
 
     def populateBoard(self):
         for i in range(self.width):
             for j in range(self.height):
-                self.array[i][j] = Cell(i, j, True)
+                stat = False
+                rand = random.randint(0, 1)
+
+                if i == 1:
+                    stat = True
+
+                self.array[i][j] = Cell(i, j, stat)
 
     def printBoard(self):
         for i in range(self.width):
@@ -24,9 +32,66 @@ class Board:
                 else:
                     print("X", end=" ")
 
-    def checkNeighbors(self):
+    def clear(self):
+        self.array = np.empty((self.width, self.height), Cell)
 
+    def getCell(self, x, y):
+        return self.array[x][y]
 
+    def checkNeighbors(self, i, j):
 
+        lifeCount = 0
 
+        # set/reset bounds for x-axis
+        if i > 0:
+            tempWidthStart = i - 1
+        else:
+            tempWidthStart = i
+        if i < 4:
+            tempWidthEnd = i + 1
+        else:
+            tempWidthEnd = i
+
+        # nested loops that check the number of surrounding cells
+        while tempWidthStart <= tempWidthEnd:
+
+            # set/reset bounds for y-axis
+            if j > 0:
+                tempHeightStart = j - 1
+            else:
+                tempHeightStart = j
+            if j < 5:
+                tempHeightEnd = j + 1
+            else:
+                tempHeightEnd = j
+
+            # height loop for surrounding cells
+
+            while tempHeightStart <= tempHeightEnd:
+                print(tempWidthStart, tempHeightStart)
+
+                # keeps from including the current cell in lifeCount
+                if tempWidthStart != i or tempHeightStart != j:
+
+                    # if a neighbor cell is alive it is added to lifeCount
+                    if self.array[tempWidthStart][tempHeightStart].getStatus():
+                        lifeCount += 1
+
+                tempHeightStart += 1
+            tempWidthStart += 1
+        print(lifeCount, "  ", self.array[i][j].getStatus(), i, j, end="  ")
+        if self.array[i][j].getStatus():
+            if lifeCount == 3 or lifeCount == 2:
+                print("live -- live")
+                return True
+            else:
+                print("live -- dead")
+                return False
+        else:
+            if lifeCount == 3:
+                print("dead -- live")
+                return True
+            else:
+                print("dead -- dead")
+                return False
 
--- GameOfLife/main.py
+++ GameOfLife/main.py
@@ -1,15 +1,47 @@
 from GameOfLife.Cell import Cell
 
+from GameOfLife.Board import Board
+
 import numpy as np
 
+import pygame as pygame
+
+import random
+
+(width, height) = (500, 500)
+screen = pygame.display.set_mode((width, height))
+
+
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+
+switch = BLUE
+for i in range(25):
+    for j in range(25):
+        pygame.draw.rect(screen, switch, (i*20, j*20, 18, 18))
+        if switch == WHITE:
+            switch = BLUE
+        else:
+            switch = WHITE
+
+pygame.display.flip()
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
 # board arrays to store cells
+
 board0 = np.empty((5, 6), Cell)
 board1 = np.empty((5, 6), Cell)
 
 # initializing the board
 for i in range(5):
     for j in range(6):
-        if i % 2 != 0:
+        rand = random.randint(0, 1)
+        if rand == 1:
             stat = True
         else:
             stat = False
@@ -24,73 +56,99 @@ for i in range(5):
         else:
             print("X", end=' ')
 print()
-for i in range(5):
-    for j in range(6):
-        board1[i][j] = Cell(i, j, False)
-        # counts the number of neighboring live cells
-        lifeCount = 0
+for c in range(10):
+    for i in range(5):
+        for j in range(6):
+            board1[i][j] = Cell(i, j, False)
+            # counts the number of neighboring live cells
+            lifeCount = 0
 
-        # set/reset bounds for x-axis
-        if i > 0:
-            tempWidthStart = i - 1
-        else:
-            tempWidthStart = i
-        if i < 4:
-            tempWidthEnd = i + 1
-        else:
-            tempWidthEnd = i
-
-        # nested loops that check the number of surrounding cells
-        while tempWidthStart <= tempWidthEnd:
-
-            # set/reset bounds for y-axis
-            if j > 0:
-                tempHeightStart = j - 1
+            # set/reset bounds for x-axis
+            if i > 0:
+                tempWidthStart = i - 1
             else:
-                tempHeightStart = j
-            if j < 5:
-                tempHeightEnd = j + 1
+                tempWidthStart = i
+            if i < 4:
+                tempWidthEnd = i + 1
             else:
-                tempHeightEnd = j
+                tempWidthEnd = i
 
-            # height loop for surrounding cells
-            while tempHeightStart <= tempHeightEnd:
+            # nested loops that check the number of surrounding cells
+            while tempWidthStart <= tempWidthEnd:
 
-                # keeps from including the current cell in lifeCount
-                if tempWidthStart != i or tempHeightStart != j:
+                # set/reset bounds for y-axis
+                if j > 0:
+                    tempHeightStart = j - 1
+                else:
+                    tempHeightStart = j
+                if j < 5:
+                    tempHeightEnd = j + 1
+                else:
+                    tempHeightEnd = j
 
-                    # if a neighbor cell is alive it is added to lifeCount
-                    if board0[tempWidthStart][tempHeightStart].getStatus():
-                        lifeCount += 1
+                # height loop for surrounding cells
+                while tempHeightStart <= tempHeightEnd:
 
-                tempHeightStart += 1
-            tempWidthStart += 1
-        print(lifeCount)
-        if board0[i][j].getStatus():
-            if lifeCount == 3 or lifeCount == 2:
-                board1[i][j].setStatus(True)
+                    # keeps from including the current cell in lifeCount
+                    if tempWidthStart != i or tempHeightStart != j:
+
+                        # if a neighbor cell is alive it is added to lifeCount
+                        if board0[tempWidthStart][tempHeightStart].getStatus():
+                            lifeCount += 1
+
+                    tempHeightStart += 1
+                tempWidthStart += 1
+            if board0[i][j].getStatus():
+                if lifeCount == 3 or lifeCount == 2:
+                    board1[i][j].setStatus(True)
+                else:
+                    board1[i][j].setStatus(False)
+            elif not board0[i][j].getStatus():
+                if lifeCount == 3:
+                    board1[i][j].setStatus(True)
+                else:
+                    board1[i][j].setStatus(False)
+
+    board0 = board1
+    board1 = np.empty((5, 6), Cell)
+
+    for i in range(5):
+        print()
+
+        for j in range(6):
+
+            if board0[i][j].getStatus():
+                print("O", end=' ')
+
             else:
-                board1[i][j].setStatus(False)
-        elif board0[i][j].getStatus() == False:
-            if lifeCount == 3:
-                board1[i][j].setStatus(True)
-            else:
-                board1[i][j].setStatus(False)
-
-
-
-
-
-for i in range(5):
+                print("X", end=' ')
     print()
 
+print("*************")
+print()
+
+new_board = Board(5, 6)
+new_board1 = Board(5, 6)
+
+new_board.clear()
+
+new_board1.populateBoard()
+
+new_board.populateBoard()
+
+
+new_board.printBoard()
+
+for i in range(5):
     for j in range(6):
 
-        if board1[i][j].getStatus():
-            print("O", end=' ')
+        new_board1.getCell(i, j).setStatus(new_board.checkNeighbors(i, j))
 
-        else:
-            print("X", end=' ')
+new_board = new_board1
+print()
+
+new_board.printBoard()
+
 
 
 
