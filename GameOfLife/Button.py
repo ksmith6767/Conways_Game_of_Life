4b03c4f645938647739f2367d23e6ce41b4d37ba
Kyle Smith <ksmith126@ycp.edu>
2021-04-10 12:16:52 -0400

    Created buttons that do functions and highlight

---
+++ GameOfLife/Button.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Button:
+
+    # some of this code is based on a tutorial I found online for creating text buttons
+
+    WHITE = (255, 255, 255)
+
+    def __init__(self, color, x, y, width, height, text):
+        self.color = color
+        self.x = x
+        self.y = y
+        self.width = width
+        self. height = height
+        self.text = text
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
+
+        if self.text != '':
+            font = pygame.font.SysFont('arial', 35, 0, 0)
+            text = font.render(self.text, 1, (255, 255, 255))
+            window.blit(text, (self.x + (self.width/2 - text.get_width()/2), self.y + (self.height/2 - text.get_height()/2)))
+
+    def over(self, pos):
+        if pos[0] > self.x and pos[0] < self.x + self.width:
+            if pos[1] > self.y and pos[1] < self.y + self.height:
+                return True
+        return False
+
--- GameOfLife/main.py
+++ GameOfLife/main.py
@@ -1,15 +1,21 @@
 
 from GameOfLife.Board import Board
 
+from GameOfLife.Button import Button
+
 import pygame
 
+pygame.init()
+
 # define the screen as 500X500 pixels
-(width, height) = (500, 500)
+(width, height) = (750, 750)
 screen = pygame.display.set_mode((width, height))
 
 # color definitions
-WHITE = (50, 50, 50)
+GREY = (50, 50, 50)
+LIGHTGREY = (70, 70, 70)
 BLUE = (0, 50, 205)
+LIGHTBLUE = (0, 70, 225)
 
 # define the bounds of the board
 WIDTH = 25
@@ -27,18 +33,34 @@ board1 = Board(WIDTH, HEIGHT)
 board.populateBoard()
 board1.clear()
 
+# repopulate button
+newbutton = Button(GREY, 25, 525, 200, 50, 'Repopulate')
+# step button
+newbutton2 = Button(GREY, 300, 525, 200, 50, 'Step')
+# clear button
+newbutton3 = Button(GREY, 25, 600, 200, 50, 'Clear')
+
+
+
 # MAIN GAME LOOP
 run = True
 while run:
 
-    #    block to draw the representation of the board
+    screen.fill((35, 35, 35))
+
+    newbutton.draw(screen)
+    newbutton2.draw(screen)
+    newbutton3.draw(screen)
+
+
+    # block to draw the representation of the board
     for i in range(WIDTH):
         for j in range(HEIGHT):
 
             if board.getCell(i, j).getStatus():
                 switch = BLUE
             else:
-                switch = WHITE
+                switch = GREY
 
             pygame.draw.rect(screen, switch, (i * 20, j * 20, 18, 18))
 
@@ -59,20 +81,61 @@ while run:
                     for j in range(HEIGHT):
                         board.getCell(i, j).setStatus(board1.getCell(i, j).getStatus())
 
+        if event.type == pygame.MOUSEMOTION:
+            if newbutton.over(pygame.mouse.get_pos()):
+                newbutton.color = LIGHTGREY
+            else:
+                newbutton.color = GREY
+
+            if newbutton2.over(pygame.mouse.get_pos()):
+                newbutton2.color = LIGHTGREY
+            else:
+                newbutton2.color = GREY
+
+            if newbutton3.over(pygame.mouse.get_pos()):
+                newbutton3.color = LIGHTGREY
+            else:
+                newbutton3.color = GREY
+
+
+
         if event.type == pygame.MOUSEBUTTONUP:
             x, y = pygame.mouse.get_pos()
-            print(x, y)
-            x = int(x/20)
-            y = int(y/20)
-            print(x, y)
-            if not board.getCell(x, y).getStatus():
-                board.getCell(x, y).setStatus(True)
-            else:
-                board.getCell(x, y).setStatus(False)
+            mouse_pos = event.pos
+            if x < 500 and y < 500:
+                print(x, y)
+                x = int(x/20)
+                y = int(y/20)
+                print(x, y)
+                if not board.getCell(x, y).getStatus():
+                    board.getCell(x, y).setStatus(True)
+                else:
+                    board.getCell(x, y).setStatus(False)
+            # button collision for random population
+            if newbutton.over(mouse_pos):
+                print("ahhhh")
+                board.populateBoard()
+            # button collision for stepping
+            if newbutton2.over(mouse_pos):
+                for i in range(WIDTH):
+                    for j in range(HEIGHT):
+                        board1.getCell(i, j).setStatus(board.checkNeighbors(i, j))
+                for i in range(WIDTH):
+                    for j in range(HEIGHT):
+                        board.getCell(i, j).setStatus(board1.getCell(i, j).getStatus())
+
+            # button collision for clearing the board
+            if newbutton3.over(mouse_pos):
+                print("ahhhh")
+                board.clear()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
                 print("press r")
                 board.populateBoard()
 
-    pygame.time.delay(100)
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_c:
+                board.clear()
+
+    pygame.time.delay(10)
